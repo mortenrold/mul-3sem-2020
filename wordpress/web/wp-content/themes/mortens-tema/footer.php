21143175170be86e471526dc6f6c6941601a7b49
Morten Rold <moro@cphbusiness.dk>
2020-10-01 11:02:08 +0200

    01.10.20

--- wordpress/web/wp-content/themes/mortens-tema/footer.php
+++ wordpress/web/wp-content/themes/mortens-tema/footer.php
@@ -13,17 +13,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mortens-tema' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'mortens-tema' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'mortens-tema' ), 'mortens-tema', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+			<p>&copy; Lavet af Morten</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
